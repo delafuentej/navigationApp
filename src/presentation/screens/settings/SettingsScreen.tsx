a2793f9e6a1c7d925f32bc69f6a498c00a9b0b80
delafuentej <jfuentelucas@gmail.com>
2024-06-29 10:49:39 +0200

    button go back/ go to home

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,11 +1,23 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
 import { Text, View } from 'react-native';
+import { globalStyles } from '../../theme/theme';
+import { PrimaryButton } from '../../components/shared/PrimaryButton';
+import { StackActions, useNavigation } from '@react-navigation/native';
 
 export const SettingsScreen = () => {
+  const navigator = useNavigation();
   return (
-    <View>
-        <Text>SettingsScreen</Text>
+    <View style={globalStyles.container}>
+        <Text>Settings Screen</Text>
+        <PrimaryButton
+          onPress={()=> navigator.goBack()}
+          label='Go back'
+        />
+         <PrimaryButton
+          onPress={()=> navigator.dispatch(StackActions.popToTop())}
+          label='Go to Home'
+        />
     </View>
   );
 };
