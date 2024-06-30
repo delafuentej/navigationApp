1f010e172235329a80fdf2eaf6599e898bf1107e
delafuentej <jfuentelucas@gmail.com>
2024-06-30 11:28:13 +0200

    icons installed

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -5,12 +5,16 @@
 
 import {Text, View } from 'react-native';
 import { HamburgerMenu } from '../../components/shared/HamburgerMenu';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { globalColors} from '../../theme/theme';
+
 
 export const Tab1Screen = () => {
   return (
     <View>
         <HamburgerMenu />
         <Text>Tab1Screen</Text>
+        <Text> <Icon name="rocket-outline" size={50} color={globalColors.white} /></Text>
     </View>
   );
 };
